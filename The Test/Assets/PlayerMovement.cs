f8f9a090316f34f712e83b33510d76ffac14ca66
Sagerno <137656650+Sagerno@users.noreply.github.com>
2023-06-25 13:34:59 +0100

    s1

--- The Test/Assets/PlayerMovement.cs
+++ The Test/Assets/PlayerMovement.cs
@@ -6,11 +6,17 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 5f;  // Force applied when jumping
     public bool isGrounded = false;  // Flag to track if the capsule is grounded
 
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 10f;
+
     private Rigidbody rb;
+    private bool isSprinting = false;
+    private float currentSpeed;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentSpeed = walkSpeed;
     }
 
     private void Update()
@@ -22,8 +28,20 @@ public class PlayerMovement : MonoBehaviour
         // Calculate movement direction
         Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
+        // Check if the Shift key is held down
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            isSprinting = false;
+            currentSpeed = walkSpeed;
+        }
+
         // Apply movement
-        transform.Translate(movementDirection * movementSpeed * Time.deltaTime);
+        transform.Translate(movementDirection * currentSpeed * Time.deltaTime);
 
         // Jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
